26e9ad85232ca82f71063f1b695910cdf405c137
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-28 18:45:07 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# The data is read into a numpy array called radii
+radii = np.loadtxt("bubbles.dat")
+
+# To complete the exercise you need to use the instructions on the 
+# right to compute these quantities correctly from the input data.
+xbar = 0
+
+
+print("The average radius of the bubbles is", xbar)
